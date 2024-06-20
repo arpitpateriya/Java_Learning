307b726587b5ba606b2f65a50c574a25eba91930
arpitpateriya <arpitpateriya14@gmail.com>
2024-06-20 18:56:44 +0530

    java practice

---
+++ src/Hello_world.java
@@ -0,0 +1,16 @@
+class Hello_world
+{
+    public static void main(String args[])
+    {
+        System.out.println("\n\t\t\t\t\t\t\t\t\t\tit is hot.");
+        System.out.println("\t\t\t\t\t\t\t\t\t\tThe sky is blue.");
+        System.out.println("\t\t\t\t\t\t\t\t\t\tA little cloud comes looking for you");
+        System.out.println("\t\t\t\t\t\t\t\t\t\tMore clouds come");
+        System.out.println("");
+        System.out.println("\t\t\t\t\t\t\t\t\t\tthey bring rain");
+        System.out.println("\t\t\t\t\t\t\t\t\t\tsing and dance");
+        System.out.println("\t\t\t\t\t\t\t\t\t\tit's cool again");
+        System.out.println("\t\t\t\t\t\t\t\t\t\tThank you !");
+
+    }
+}
---
+++ src/Parameterized.java
@@ -0,0 +1,14 @@
+class ss1{
+
+    ss1(int x)
+    {
+        System.out.println(x);
+    }
+}
+public class Parameterized {
+    public static  void main(String args [])
+    {
+        ss1 s = new ss1(10);
+
+    }
+}
